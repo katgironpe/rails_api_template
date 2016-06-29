f1b3ab2f94e45b5c7ab249e76f38f068537e2a1e
Katherine G. Pe <katherinepe@me.com>
2016-06-29 19:18:21 +0800

    Added shoulda

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :roles
 
+  validates :first_name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
+
+  def name
+    [first_name, last_name].join(' ')
+  end
 end
