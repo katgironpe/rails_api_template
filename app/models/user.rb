1f528e893f6d11b2b4e7507190728d5e3d3a093b
Katherine G. Pe <katherinepe@me.com>
2016-06-29 19:29:32 +0800

    Added more validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_and_belongs_to_many :roles
 
   validates :first_name, presence: true
+  validates :last_name, presence: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
