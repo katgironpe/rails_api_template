0100b1c5f13d2002f6948d1584328fec0e09a7f4
Katherine G. Pe <katherinepe@me.com>
2016-07-06 17:42:42 +0800

    Test associations

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 describe Role do
+  it { should have_and_belong_to_many(:users).with_foreign_key(:role_id) }
+
   it { should validate_presence_of(:name) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe User do
+  it { should have_and_belong_to_many(:roles).with_foreign_key(:user_id) }
+
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
 end
